2aac7fd8fc1d649422d664739ea5f3ab90655659
sadt <suryaadtmaja13@gmail.com>
2021-04-10 18:49:11 +0700

    sidebar component

---
+++ src/views/Sidebar.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <div class="w-full flex">
+      <sidebar />
+    </div>
+  </main>
+</template>
+<script>
+import Sidebar from "@/components/Sidebar.vue";
+
+export default {
+  components: { Sidebar },
+};
+</script>
